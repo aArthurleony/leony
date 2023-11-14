90b77f932f5426f1cbc0c0531f12c910d35a3d51
Arthur Leony <leonyalvesarthuroliveira263@gmail.com>
2023-11-14 12:43:42 -0300

    aa

--- EventosDOM/projeto01/script.js
+++ EventosDOM/projeto01/script.js
@@ -1,4 +1,13 @@
 function verificarmulta(){
-    let velocidade = document.querySelector('#velocidade').value
-    console.log(velocidade)
+    const velocidade = document.querySelector('#velocidade').value
+    const resultado = document.getElementById('resultado')
+
+    if(velocidade <= 60){
+        resultado.innerHTML = "Você não foi multado"
+        resultado.className = "verde"
+    }else{
+        resultado.innerHTML = "você foi multado"
+        resultado.className = "vermelho"
+
+    }
 }
