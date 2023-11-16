634a5198b1ae4ed32c9ab70fad1b250baafd4e22
Arthur Leony <leonyalvesarthuroliveira263@gmail.com>
2023-11-16 10:57:55 -0300

    novo projeto

---
+++ EventosDOM/Contador/app.js
@@ -0,0 +1,25 @@
+let count = 0
+
+const value = document.getElementById('value')
+const btns = document.querySelectorAll('.btn')
+
+// console.log(value)
+// console.log(btns)
+
+btns.forEach(function(btn){
+    
+    btn.addEventListener('click', function(){
+        const style = event.currentTarget.classList
+        if(style.contains('decrease')){
+            count--;
+        }else if(style.contains('reset')){
+            count = 0;
+        }else if(style.contains('increase')){
+            count++;
+        }
+            
+        value.textContent = count
+
+    })
+
+})
