b37567eea00c7ed88e6a6721c9874051bd90e27f
Arthur Leony <leonyalvesarthuroliveira263@gmail.com>
2023-11-21 12:29:14 -0300

    add

---
+++ miniapps/formulario/app.js
@@ -0,0 +1,24 @@
+function validaformulario (){
+    const nome = document.getElementById("nome").value;
+    const email = document.querySelector("#email").value;
+    const senha = document.getElementById("senha").value;
+    
+    /*
+    validação dos campos preenchidos
+    */
+
+    if(nome == '' || email == '' || senha == ''){
+        alert("Por favor, preencha os campos")
+    }
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+        if(!emailRegex.test(email)){
+            alert("Por favor, insira um e-mail válido!")
+            return
+    } 
+    //validação da senha
+    if(senha.length > 8){
+        alert("Limite de carácteres excedido!")
+        return
+    }
+     
+}
