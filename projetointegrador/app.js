e5311433390612d247ba0be1b415e8e6100af5a5
Arthur Leony <leonyalvesarthuroliveira263@gmail.com>
2023-11-23 12:27:44 -0300

    aaa

---
+++ projetointegrador/app.js
@@ -0,0 +1,14 @@
+//  ---Gerenciamento de Reservas 
+
+// hospede
+let nome,email,numTelefone,MetdPagamento=0
+
+// reserva
+let dataCheckIn,dataCheckOut,tipoQuarto,RequisitosEspeciais=0
+
+// status
+let hitoricoReservas,status=0
+
+hitoricoReservas= dataCheckIn + dataCheckOut + tipoQuarto + status
+
+// 
